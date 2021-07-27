4dd6e192a2c6cde673a8927bed4f65ae7f30ca03
Maksym Kharchenko <mkharchenko@microsoft.com>
2021-07-27 18:39:09 +0200

    add cases

---
+++ playground/cases/basic-nested-props-change.tsx
@@ -0,0 +1,28 @@
+import * as React from "react";
+import { useTrackRender } from "../helpers";
+import { TestCase } from "../types";
+
+function ChildWrapper() {
+  const { useState } = useTrackRender();
+  const [mounted, setMounted] = useState("Fail: waiting for mount");
+
+  React.useEffect(() => {
+    setMounted("OK");
+  }, []);
+
+  return <Child mounted={mounted} />;
+}
+
+function Child({ mounted }: { mounted: string }) {
+  useTrackRender();
+  return <>{mounted}</>;
+}
+
+function Root() {
+  return <ChildWrapper />;
+}
+
+export default {
+  title: "Basic render with changed props via useEffect() and useState",
+  Root,
+} as TestCase;
--- playground/cases/index.ts
+++ playground/cases/index.ts
@@ -2,5 +2,7 @@ import { TestCase } from "../types";
 import { default as case1 } from "./basic";
 import { default as case2 } from "./basic-nested-set-state";
 import { default as case3 } from "./basic-context-change";
+import { default as case4 } from "./basic-nested-props-change";
+import { default as case5 } from "./basic-parent-element-change";
 
-export default [case1, case2, case3] as TestCase[];
+export default [case1, case2, case3, case4, case5] as TestCase[];
