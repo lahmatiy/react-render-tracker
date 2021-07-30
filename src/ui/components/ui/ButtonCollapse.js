09afb2a15bd7fb5a10c7f7007d463d5a9e5877cd
Roman Dvornov <rdvornov@gmail.com>
2021-07-31 01:18:54 +0200

    Tweak UI

--- src/ui/components/ui/ButtonCollapse.js
+++ src/ui/components/ui/ButtonCollapse.js
@@ -3,10 +3,14 @@ import React from "react";
 import ChevronDown from "react-feather/dist/icons/chevron-down";
 
 const ButtonCollapse = ({ isCollapsed, onToggle }) => {
+  const collapsedCls = isCollapsed ? "open" : "";
+  const disabledCls = typeof onToggle === "function" ? "" : "disabled";
+
   return (
     <button
-      className={`tree-element__toggle ${isCollapsed ? "open" : ""}`}
-      onClick={onToggle} >
+      className={`tree-element__toggle ${collapsedCls} ${disabledCls}`}
+      onClick={onToggle}
+    >
       <ChevronDown />
     </button>
   );
