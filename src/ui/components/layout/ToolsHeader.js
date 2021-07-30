a6c0040d3d9ad489fbc99b9a70246d2ee14b6bf3
Roman Dvornov <rdvornov@gmail.com>
2021-07-30 12:13:02 +0200

    Fix icon import

--- src/ui/components/layout/ToolsHeader.js
+++ src/ui/components/layout/ToolsHeader.js
@@ -1,16 +1,26 @@
 import React from "react";
 
-import { Code } from "react-feather";
+import Code from "react-feather/dist/icons/code";
 
 import FilterComponents from "../form/FilterComponents";
 import ButtonToggle from "../ui/ButtonToggle";
 
-const ToolsHeader = ({ setSearched, searched, onShowDisabled, showDisabled }) => {
+const ToolsHeader = ({
+  setSearched,
+  searched,
+  onShowDisabled,
+  showDisabled,
+}) => {
   return (
     <div className="tools-header">
       <div>
         <FilterComponents onChange={setSearched} value={searched} />
-        <ButtonToggle Icon={Code} isActive={showDisabled} onChange={onShowDisabled} tooltip="Toggle unmounted elements" />
+        <ButtonToggle
+          Icon={Code}
+          isActive={showDisabled}
+          onChange={onShowDisabled}
+          tooltip="Toggle unmounted elements"
+        />
       </div>
     </div>
   );
