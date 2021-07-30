efec79f01759507377a1aba59710bf712709f408
Dana Janoskova <danaj@microsoft.com>
2021-07-30 10:20:11 +0200

    table with details

--- src/ui/components/changes/ChangeDetails.js
+++ src/ui/components/changes/ChangeDetails.js
@@ -1,40 +1,34 @@
 import React, { useState } from "react";
-import dateFormat from "dateformat";
 
-import ButtonCollapse from "../ui/ButtonCollapse";
 import ChangeRowsHooks from "./ChangeRowsHooks";
 
 const reasons = ["props", "state", "hooks"];
 
 const ChangeDetails = ({ details }) => {
-  const [isCollapsed, setIsCollapsed] = useState(false);
-
-  const handleToggle = () => {
-    setIsCollapsed(prev => !prev);
-  };
-
   return (
-    <>
-      {reasons.map(reason => {
-        if (details[reason]?.length) {
-          const data = details[reason];
+    <tr>
+      <td colSpan={4}>
+        {reasons.map(reason => {
+          if (details[reason]?.length) {
+            const data = details[reason];
 
-          switch (reason) {
-            case "hooks": {
-              return <ChangeRowsHooks data={data} />;
+            switch (reason) {
+              case "hooks": {
+                return <ChangeRowsHooks data={data} key={reason} />;
+              }
+              case "state": {
+                return <ChangeRowsHooks data={data} key={reason} />;
+              }
+              case "props": {
+                return <ChangeRowsHooks data={data} key={reason} />;
+              }
+              default:
+                return null;
             }
-            case "state": {
-              return <ChangeRowsHooks data={data} />;
-            }
-            case "props": {
-              return <ChangeRowsHooks data={data} />;
-            }
-            default:
-              return null;
           }
-        }
-      })}
-    </>
+        })}
+      </td>
+    </tr>
   );
 };
 
--- src/ui/components/changes/ChangeRowsHooks.js
+++ src/ui/components/changes/ChangeRowsHooks.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React from "react";
 
 const ChangeRowsHooks = ({ data }) => {
-
+  return (
+    <table>
+      <thead>
+      <tr>
+        <th>Name</th>
+        <th>Previous</th>
+        <th>Next</th>
+      </tr>
+      </thead>
+      <tbody>
+      {data.map(row => {
+        console.log(row);
+        return (
+          <tr key={row.index}>
+            <td>{row.name}</td>
+            <td>{JSON.stringify(row.prev)}</td>
+            <td>{JSON.stringify(row.next)}</td>
+          </tr>
+        );
+      })}
+      </tbody>
+    </table>
+  );
 };
 
 export default ChangeRowsHooks;
