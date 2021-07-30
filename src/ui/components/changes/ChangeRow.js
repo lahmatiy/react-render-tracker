c33448d2edeaab05c933c35fc720d36a5434baee
Yury Tomilin <yurytomilin@microsoft.com>
2021-07-30 14:26:20 +0200

    Show element ID in change list

--- src/ui/components/changes/ChangeRow.js
+++ src/ui/components/changes/ChangeRow.js
@@ -3,8 +3,9 @@ import dateFormat from "dateformat";
 
 import ButtonCollapse from "../ui/ButtonCollapse";
 import ChangeDetails from "./ChangeDetails";
+import ElementId from "../element/ElementId";
 
-const ChangeRow = ({ event, timestamp, displayName }) => {
+const ChangeRow = ({ event, timestamp, displayName, elementId }) => {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const handleToggle = () => {
@@ -17,7 +18,11 @@ const ChangeRow = ({ event, timestamp, displayName }) => {
         <td>
           <ButtonCollapse isCollapsed={isCollapsed} onToggle={handleToggle} />
         </td>
-        {displayName && <td>{displayName}</td>}
+        {displayName && (
+          <td>
+            {displayName} <ElementId id={elementId} />
+          </td>
+        )}
         <td>{dateFormat(timestamp, "h:MM:ss.l TT")}</td>
         <td>{event.phase}</td>
         <td>{event.reason?.join(", ")}</td>
