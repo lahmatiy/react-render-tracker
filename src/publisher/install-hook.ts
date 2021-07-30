822429a1b7bcb8119768d72dbc3fbe12b73373b3
Roman Dvornov <rdvornov@gmail.com>
2021-07-30 23:32:34 +0200

    Make tool work when React devtools enabled

--- src/publisher/install-hook.ts
+++ src/publisher/install-hook.ts
@@ -5,23 +5,35 @@ import { DevtoolsHook } from "./devtools-hook";
  * {@link packages/react-reconciler/src/ReactFiberDevToolsHook.new.js L:44}
  */
 const hookName = "__REACT_DEVTOOLS_GLOBAL_HOOK__";
+const MARKER = Symbol();
 
 export function installHook(target) {
+  const existingHook = target[hookName];
+
   if (target.hasOwnProperty(hookName)) {
-    return null;
+    if (existingHook[MARKER] === MARKER) {
+      return existingHook;
+    }
   }
 
   const hook = new DevtoolsHook();
 
-  Object.defineProperty(target, hookName, {
-    // This property needs to be configurable for the test environment,
-    // else we won't be able to delete and recreate it between tests.
-    configurable: true,
-    enumerable: false,
-    get() {
-      return hook;
-    },
-  });
+  if (existingHook) {
+    for (const key of Object.keys(existingHook)) {
+      delete existingHook[key];
+    }
+    Object.assign(existingHook, hook, { [MARKER]: MARKER });
+  } else {
+    Object.defineProperty(target, hookName, {
+      // This property needs to be configurable for the test environment,
+      // else we won't be able to delete and recreate it between tests.
+      configurable: true,
+      enumerable: false,
+      get() {
+        return hook;
+      },
+    });
+  }
 
-  return hook;
+  return target[hookName];
 }
