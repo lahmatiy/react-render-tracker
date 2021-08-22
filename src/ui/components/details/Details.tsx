64dcdd567b38ef58598e8385fc4c51dab895b89e
Roman Dvornov <rdvornov@gmail.com>
2021-08-22 16:12:00 +0200

    Add show more buttons for a very long event logs

--- src/ui/components/details/Details.tsx
+++ src/ui/components/details/Details.tsx
@@ -1,52 +1,15 @@
 import React, { useState } from "react";
 import List from "react-feather/dist/icons/list";
 import ButtonToggle from "../common/ButtonToggle";
-import { TreeElement, ElementEvent } from "../../types";
+import { TreeElement } from "../../types";
 import EventList from "./EventList";
 
 interface DetailsProps {
   data: TreeElement;
 }
 
-function getEventLog(component: TreeElement, showChildChanges = false) {
-  if (showChildChanges) {
-    const queue = new Set([component]);
-    const combinedChanges: ElementEvent[] = [];
-
-    for (const component of queue) {
-      for (const event of component.events) {
-        combinedChanges.push({
-          component,
-          event,
-        });
-      }
-
-      if (component.children) {
-        for (const child of component.children) {
-          queue.add(child);
-        }
-      }
-    }
-
-    return combinedChanges.sort((a, b) => a.event.id - b.event.id);
-  } else {
-    return component.events.map(event => ({
-      component,
-      event,
-    }));
-  }
-}
-
 const Details = ({ data }: DetailsProps) => {
   const [showChildChanges, setShowChildChanges] = useState(true);
-  const records = getEventLog(data, showChildChanges);
-  let content = null;
-
-  if (!records.length) {
-    content = "No events found";
-  } else {
-    content = <EventList records={records} />;
-  }
 
   return (
     <div className="details">
@@ -58,7 +21,11 @@ const Details = ({ data }: DetailsProps) => {
           tooltip="Show child changes"
         />
       </div>
-      {content}
+      <EventList
+        key={data.id} // to reset state of visible records on component change
+        data={data}
+        showChildChanges={showChildChanges}
+      />
     </div>
   );
 };
--- src/ui/components/details/EventList.tsx
+++ src/ui/components/details/EventList.tsx
@@ -1,20 +1,80 @@
-import React from "react";
-import { ElementEvent } from "../../types";
+import * as React from "react";
+import { ElementEvent, TreeElement } from "../../types";
 import EventListItem from "./EventListItem";
 
 interface EventListProps {
-  records: ElementEvent[];
+  data: TreeElement;
+  showChildChanges: boolean;
 }
 
-const EventList = ({ records }: EventListProps) => {
+function getEventLog(component: TreeElement, showChildChanges = false) {
+  if (showChildChanges) {
+    const queue = new Set([component]);
+    const combinedChanges: ElementEvent[] = [];
+
+    for (const component of queue) {
+      for (const event of component.events) {
+        combinedChanges.push({
+          component,
+          event,
+        });
+      }
+
+      if (component.children) {
+        for (const child of component.children) {
+          queue.add(child);
+        }
+      }
+    }
+
+    return combinedChanges.sort((a, b) => a.event.id - b.event.id);
+  } else {
+    return component.events.map(event => ({
+      component,
+      event,
+    }));
+  }
+}
+
+const sectionSize = 50;
+const sectionMinSize = 10;
+const EventList = ({ data, showChildChanges }: EventListProps) => {
+  const records = getEventLog(data, showChildChanges);
+  const [startOffset, setStartOffset] = React.useState(() => {
+    const offset = Math.max(0, records.length - sectionSize);
+    return offset < sectionMinSize ? 0 : offset;
+  });
+
+  if (!records.length) {
+    return <div>No events found</div>;
+  }
+
   return (
-    <table className="element-event-list">
-      <tbody>
-        {records.map(({ component, event }) => (
-          <EventListItem key={event.id} component={component} event={event} />
-        ))}
-      </tbody>
-    </table>
+    <>
+      {startOffset > 0 && (
+        <div className="element-event-list__show-more">
+          {startOffset > sectionSize + sectionMinSize && (
+            <button
+              onClick={() =>
+                setStartOffset(Math.max(0, startOffset - sectionSize))
+              }
+            >
+              Show {Math.min(startOffset, sectionSize)} more...
+            </button>
+          )}
+          <button onClick={() => setStartOffset(0)}>
+            Show all the rest {startOffset}...
+          </button>
+        </div>
+      )}
+      <table className="element-event-list">
+        <tbody>
+          {records.slice(startOffset).map(({ component, event }) => (
+            <EventListItem key={event.id} component={component} event={event} />
+          ))}
+        </tbody>
+      </table>
+    </>
   );
 };
 
