c83a12c3b7e8624043f41755366b92052f090c5f
Roman Dvornov <rdvornov@gmail.com>
2021-08-26 02:53:51 +0200

    Move processEvents to events module

--- src/ui/utils/events.ts
+++ src/ui/utils/events.ts
@@ -4,12 +4,150 @@ import { useGlobalMaps } from "./global-maps";
 import { subscribeSubtree } from "./tree";
 import { Message, MessageElement } from "../types";
 
-export const useEventLog = (
+function upsertComponent(
+  map: Map<number, number[]>,
+  id: number,
+  componentId: number
+) {
+  if (map.has(id)) {
+    map.set(id, map.get(id).concat(componentId));
+  } else {
+    map.set(id, [componentId]);
+  }
+}
+
+function removeComponent(
+  map: Map<number, number[]>,
+  id: number,
+  componentId: number
+) {
+  if (map.has(id)) {
+    const list = map.get(id);
+    const idx = list.indexOf(componentId);
+    list.splice(idx, 1);
+  }
+}
+
+function markUpdated(map: Map<number, number>, id: number, type: number) {
+  if (map.has(id)) {
+    map.set(id, map.get(id) | type);
+  } else {
+    map.set(id, type);
+  }
+}
+
+const UPDATE_SELF /*        */ = 0b00000001;
+const UPDATE_OWNER /*       */ = 0b00000010;
+const UPDATE_PARENT /*      */ = 0b00000100;
+const UPDATE_MOUNT_OWNER /* */ = 0b00001000;
+const UPDATE_MOUNT_PARENT /**/ = 0b00010000;
+
+export function processEvents(
+  events: Message[],
+  {
+    componentById,
+    componentsByParentId,
+    componentsByOwnerId,
+    mountedComponentsByParentId,
+    mountedComponentsByOwnerId,
+  }: ReturnType<typeof useGlobalMaps>
+) {
+  const updated = new Map<number, number>();
+
+  for (const event of events) {
+    let element: MessageElement;
+
+    switch (event.op) {
+      case "mount": {
+        element = {
+          ...event.element,
+          mounted: true,
+          events: [],
+          rerendersCount: 0,
+          selfTime: event.selfTime,
+          totalTime: event.totalTime,
+        };
+
+        markUpdated(updated, element.parentId, UPDATE_OWNER);
+        upsertComponent(componentsByParentId, element.parentId, element.id);
+        markUpdated(updated, element.parentId, UPDATE_MOUNT_OWNER);
+        upsertComponent(
+          mountedComponentsByParentId,
+          element.parentId,
+          element.id
+        );
+
+        markUpdated(updated, element.ownerId, UPDATE_OWNER);
+        upsertComponent(componentsByOwnerId, element.ownerId, element.id);
+        markUpdated(updated, element.ownerId, UPDATE_MOUNT_OWNER);
+        upsertComponent(
+          mountedComponentsByOwnerId,
+          element.ownerId,
+          element.id
+        );
+        break;
+      }
+
+      case "unmount": {
+        element = {
+          ...componentById.get(event.elementId),
+          mounted: false,
+        };
+
+        markUpdated(updated, element.ownerId, UPDATE_MOUNT_OWNER);
+        removeComponent(
+          mountedComponentsByOwnerId,
+          element.ownerId,
+          element.id
+        );
+
+        markUpdated(updated, element.parentId, UPDATE_MOUNT_PARENT);
+        removeComponent(
+          mountedComponentsByParentId,
+          element.parentId,
+          element.id
+        );
+
+        break;
+      }
+
+      case "rerender":
+        element = componentById.get(event.elementId);
+        element = {
+          ...element,
+          rerendersCount: element.rerendersCount + 1,
+          selfTime: element.selfTime + event.selfTime,
+          totalTime: element.totalTime + event.totalTime,
+        };
+
+        break;
+    }
+
+    markUpdated(updated, element.id, UPDATE_SELF);
+    componentById.set(event.elementId, {
+      ...element,
+      events: element.events.concat(event),
+    });
+  }
+
+  // console.log("updated", [...updated], events);
+  for (const [id, update] of updated) {
+    update & UPDATE_SELF && componentById.notify(id);
+    update & UPDATE_OWNER && componentsByOwnerId.notify(id);
+    update & UPDATE_PARENT && componentsByParentId.notify(id);
+    update & UPDATE_MOUNT_OWNER && mountedComponentsByOwnerId.notify(id);
+    update & UPDATE_MOUNT_PARENT && mountedComponentsByParentId.notify(id);
+  }
+
+  return [...componentById.values()];
+}
+
+export function useEventLog(
   componentId: number,
   groupByParent: boolean,
   includeUnmounted: boolean,
   includeSubtree: boolean
-) => {
+) {
   const { componentById, selectChildrenMap } = useGlobalMaps();
   const subtree = React.useMemo(() => new Map<number, () => void>(), []);
   const [events, setEvents] =
@@ -62,4 +200,4 @@ export const useEventLog = (
   }, [componentId, includeSubtree, subtree, childrenMap]);
 
   return events;
-};
+}
