d4ed5cf71fffcde2379114bdde094518643f9ecd
Roman Dvornov <rdvornov@gmail.com>
2021-09-20 22:55:39 +0200

    Improve JSX element name serialization

--- src/publisher/react-integration/utils/getDisplayNameFromJsx.ts
+++ src/publisher/react-integration/utils/getDisplayNameFromJsx.ts
@@ -1,8 +1,10 @@
 import {
+  CONTEXT_SYMBOL_STRING,
   FORWARD_REF_SYMBOL_STRING,
   FRAGMENT_SYMBOL_STRING,
   LAZY_SYMBOL_STRING,
   MEMO_SYMBOL_STRING,
+  PROVIDER_SYMBOL_STRING,
 } from "./constants";
 import { getDisplayName } from "./getDisplayName";
 
@@ -13,37 +15,50 @@ import { getDisplayName } from "./getDisplayName";
 // }
 
 export function getDisplayNameFromJsx(type: any) {
-  let name = "";
-
   if (type) {
     switch (typeof type) {
       case "string":
-        name = type;
-        break;
+        return type;
 
       case "function":
-        name = getDisplayName(type);
-        break;
+        return getDisplayName(type);
+
+      case "symbol":
+        if (String(type) === FRAGMENT_SYMBOL_STRING) {
+          return "";
+        }
 
       default:
-        switch (String(type.$$typeof)) {
-          case FRAGMENT_SYMBOL_STRING:
-            name = "";
-            break;
-          case MEMO_SYMBOL_STRING:
-          //   name = resolveDisplayName(type.type);
-          //   break;
-          case FORWARD_REF_SYMBOL_STRING:
-          //   name = resolveDisplayName(type.render);
-          //   break;
-          case LAZY_SYMBOL_STRING: // _ctor
-            name = getDisplayName(type);
-            break;
-          default:
-            name = String(type.$$typeof);
+        if (type.$$typeof) {
+          switch (String(type.$$typeof)) {
+            case FRAGMENT_SYMBOL_STRING:
+              return "";
+
+            case MEMO_SYMBOL_STRING:
+            //   name = resolveDisplayName(type.type);
+            //   break;
+            case FORWARD_REF_SYMBOL_STRING:
+            //   name = resolveDisplayName(type.render);
+            //   break;
+            case LAZY_SYMBOL_STRING: // _ctor
+              return getDisplayName(type);
+
+            case PROVIDER_SYMBOL_STRING: {
+              const resolvedContext = type._context || type.context;
+              return `${getDisplayName(resolvedContext, "Context")}.Provider`;
+            }
+
+            case CONTEXT_SYMBOL_STRING: {
+              const resolvedContext = type._context || type.context;
+              return `${getDisplayName(resolvedContext, "Context")}.Provider`;
+            }
+
+            default:
+              return String(type.$$typeof);
+          }
         }
     }
   }
 
-  return name;
+  return "Unknown";
 }
