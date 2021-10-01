213fdd0f448f72e7192da58014caf546dd0e4473
Roman Dvornov <rdvornov@gmail.com>
2021-10-01 18:44:51 +0200

    Some perf tweaks

--- src/ui/index.tsx
+++ src/ui/index.tsx
@@ -9,6 +9,21 @@ const rootEl = document.createElement("div");
 document.head.appendChild(document.createElement("style")).append(__CSS__);
 document.body.appendChild(rootEl);
 
+// That's actually a hack.
+// React add 2x listeners for all known events (one for capture and one for bubbling phases),
+// and perform search for a proper fiber and event handlers on it. It turns out that
+// on each pointer move there are 4-12 handlers are firing (pointermove & mousemove
+// and optionally pointerover, pointerout, mouseover, mouseout). Currently, we don't use
+// such event handlers, so avoid adding listeners for them to improve hover performance.
+const rootElAddEventListener = rootEl.addEventListener;
+rootEl.addEventListener = (
+  ...args: Parameters<typeof rootElAddEventListener>
+) => {
+  if (!/^(pointer|mouse)/.test(args[0])) {
+    rootElAddEventListener.call(rootEl, ...args);
+  }
+};
+
 // render React app
 ReactDOM.render(
   <FiberMapsContextProvider>
