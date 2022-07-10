6f22021f99f879c6ec159d6302864c08cbfd1d7c
Roman Dvornov <rdvornov@gmail.com>
2022-07-10 15:56:26 +0200

    Fix data-client works on gh-pages

--- scripts/build-gh-pages.js
+++ scripts/build-gh-pages.js
@@ -1,16 +1,25 @@
 const { copyFileSync, mkdirSync } = require("fs");
-const { buildPlayground, buildBundle } = require("./build");
+const {
+  buildPlayground,
+  buildBundle,
+  buildSelfSubscriber,
+} = require("./build");
 
 mkdirSync(".gh-pages", { recursive: true });
 copyFileSync("playground/index.html", ".gh-pages/index.html");
 copyFileSync("playground/index.css", ".gh-pages/index.css");
-buildPlayground({
-  logLevel: "info",
-  write: true,
-  outfile: ".gh-pages/index.js",
-});
-buildBundle({
-  logLevel: "info",
-  write: true,
-  outfile: ".gh-pages/react-render-tracker.js",
-});
+copyFileSync(
+  "playground/data-client-example.js",
+  ".gh-pages/data-client-example.js"
+);
+build(buildPlayground, ".gh-pages/index.js");
+build(buildBundle, ".gh-pages/react-render-tracker.js");
+build(buildSelfSubscriber, ".gh-pages/rrt-data-client.js");
+
+function build(subject, outfile) {
+  subject({
+    logLevel: "info",
+    write: true,
+    outfile,
+  });
+}
